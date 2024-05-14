b21397515deec68e4038db067166c0becba63803
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-05-14 17:39:23 +0700

    Added config

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const CURRENT_LEAGUE = 'Necropolis';
+
+export enum AppRoute {
+  Main = '/',
+  Add = '/add',
+  Edit = '/edit',
+  Login = '/login',
+}
+
+export const SORT_TAB = ['name', 'damage', 'difficulty'];
