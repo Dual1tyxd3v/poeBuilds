805f81465928ce70a1140225536f8cae352ddea0
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-05-15 15:51:06 +0700

    Refactoring

--- src/components/BuildHeader.tsx
+++ src/components/BuildHeader.tsx
@@ -1,17 +1,10 @@
 import styled from 'styled-components';
+import Header from '../ui/CardHeader';
 
 type BuildHeaderProps = {
   name: string;
   pob: string;
 };
-
-const Header = styled.div`
-  height: 5.4rem;
-  background: url(src/assets/head--left.png) top left no-repeat, url(src/assets/head--right.png) top right no-repeat,
-    url(src/assets/head--mid.png) center top repeat-x;
-  font-family: 'FontinCard';
-  text-align: center;
-`;
 
 const Title = styled.h3`
   font-size: 2.4rem;
---
+++ src/ui/CardHeader.tsx
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+const Header = styled.div`
+  height: 5.4rem;
+  background: url(src/assets/head--left.png) top left no-repeat, url(src/assets/head--right.png) top right no-repeat,
+    url(src/assets/head--mid.png) center top repeat-x;
+  font-family: 'FontinCard';
+  text-align: center;
+`;
+
+export default Header;
